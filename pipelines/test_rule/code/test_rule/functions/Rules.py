ecef7eee5ef0515e56a2d2d94ef553e779784e86
natetongwong@gmail.com <natetongwong@gmail.com>
2024-09-09 06:19:29 +0000

    Updates test_rule pipeline with new prophecy-libs version and enhanced rule functions

--- pipelines/test_rule/code/test_rule/functions/Rules.py
+++ pipelines/test_rule/code/test_rule/functions/Rules.py
@@ -1,25 +1,32 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from prophecy.utils import *
 
-def Rule1_ParseMaturityDate(maturity_date: Column=col("maturity_date")):
+@execute_rule
+def Rule1_ParseMaturityDate(maturity_date: Column=lambda: col("maturity_date")):
     return when(maturity_date.isNull(), substring(col("security_name"), - 4, 4))\
         .otherwise(maturity_date)\
         .alias("maturity_date")
 
-def Rule2_DefaultLocation(location: Column=col("location")):
+@execute_rule
+def Rule2_DefaultLocation(location: Column=lambda: col("location")):
     return when(location.isNull(), lit("AUS")).otherwise(location).alias("location")
 
-def Rule3_DefaultGL_Level_1(gl_level_1: Column=col("GL_Level_1")):
+@execute_rule
+def Rule3_DefaultGL_Level_1(gl_level_1: Column=lambda: col("GL_Level_1")):
     return when(gl_level_1.isNull(), lit("bank001")).otherwise(gl_level_1).alias("GL_Level_1")
 
-def Rule4_DefaultGL_Level_2(gl_level_2: Column=col("GL_Level_2")):
+@execute_rule
+def Rule4_DefaultGL_Level_2(gl_level_2: Column=lambda: col("GL_Level_2")):
     return when(gl_level_2.isNull(), lit("branch001")).otherwise(gl_level_2).alias("GL_Level_2")
 
-def Rule5_DefaultGL_Level_3(gl_level_3: Column=col("GL_Level_3")):
+@execute_rule
+def Rule5_DefaultGL_Level_3(gl_level_3: Column=lambda: col("GL_Level_3")):
     return when(gl_level_3.isNull(), lit("Desk1")).otherwise(gl_level_3).alias("GL_Level_3")
 
-def Rule6_ParseCouponRate(security_name: Column=col("security_name")):
+@execute_rule
+def Rule6_ParseCouponRate(security_name: Column=lambda: col("security_name")):
     return when(
           security_name.like("%\\%%"),
           concat(expr("substring(security_name, (locate('%', security_name) - 4), 4)"), lit("%"))
@@ -27,5 +34,6 @@ def Rule6_ParseCouponRate(security_name: Column=col("security_name")):
         .otherwise(lit(None))\
         .alias("coupon_rate")
 
-def Rule7_ExcludeFlag(trade_type: Column=col("TRADE_TYPE")):
+@execute_rule
+def Rule7_ExcludeFlag(trade_type: Column=lambda: col("TRADE_TYPE")):
     return when((trade_type == lit("loan")), lit("Y")).otherwise(lit("N")).alias("EXCLUDE")
