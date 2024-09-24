f3e3e582a1c48e9383d3a2250a93482894374774
natetongwong@gmail.com <natetongwong@gmail.com>
2024-09-24 06:57:37 +0000

    [Prophecy] Release version 1.6

--- gems/setup.py
+++ gems/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 packages_to_include = find_packages(exclude = ['test.*', 'test', 'test_manual'])
 setup(
     name = 'westpacbusiness_rulesettreasurystockavailability',
-    version = '1.5',
+    version = '1.6',
     packages = packages_to_include,
     description = '',
     install_requires = [],
